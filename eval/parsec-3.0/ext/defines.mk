924418e2b3dd710a21a323d9b595edca2148c3ff
Terry Hsu <hsuchi@162-1.bfc.labs.hpecorp.net>
2015-07-15 13:51:25 -0700

    Added eval script for splash2 and a global defines.mk for dthread and nvthread library path.

---
+++ eval/parsec-3.0/ext/defines.mk
@@ -0,0 +1,4 @@
+SPLASH_HOME = $(PWD)/../../../..
+NVTHREAD_HOME = $(SPLASH_HOME)/../../../..
+NVTHREAD_LIB = $(NVTHREAD_HOME)/src/libnvthread.so
+DTHREAD_LIB = $(NVTHREAD_HOME)/third-parties/dthreads/src/libdthread.so
