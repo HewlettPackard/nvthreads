1a38a1647d9d4889fbafd77f7aa12bfdbf748c0e
Terry Hsu <hsuchi@162-1.bfc.labs.hpecorp.net>
2015-07-17 15:51:37 -0700

    Added support for selecting and generating input sizes for Parsec benchmarks.

--- eval/parsec-3.0/eval_parsec.py
+++ eval/parsec-3.0/eval_parsec.py
@@ -3,8 +3,13 @@
 import os
 import re
 import sys
+import subprocess
 
 print (sys.version)
+
+#input_size = 'simlarge'
+input_size = 'native'
+runs = 1
 
 pwd = os.getcwd()
 
@@ -41,9 +46,10 @@ for c in configs:
 print
 data = {}
 
-def simlarge():
+cores = 12
+
+def sim():
 	# Let's run benchmarks (simlarge)
-	cores = 12
 	for bench in benchmarks:
 		data[bench] = {}
 		print '----------------' + bench + '----------------'
@@ -57,36 +63,63 @@ def simlarge():
 			if bench == 'blackscholes':
 				exe = pwd+'/pkgs/apps/'+bench+'/obj/amd64-linux.gcc/'+bench+suffix
 				args = str(cores)
-				inp = pwd+'/pkgs/apps/blackscholes/run/in_64K.txt'
+				inp = pwd+'/pkgs/apps/blackscholes/run/'
+				if input_size == 'simlarge':
+					inp += 'in_64K.txt'
+				elif input_size == 'native':
+					inp += 'in_10M.txt'
 				outp = pwd+'/pkgs/apps/blackscholes/run/prices.txt'
 				cmd = exe + ' ' + args + ' ' + inp + ' ' + outp
+
 			elif bench == 'canneal':
 				exe = pwd+'/pkgs/kernels/'+bench+'/obj/amd64-linux.gcc/'+bench+suffix
-				args = ' 12 15000 2000 '
-				inp = pwd+'/pkgs/kernels/canneal/run/400000.nets 128'
+				args = str(cores) + ' 15000 2000 '
+				inp = pwd+'/pkgs/kernels/canneal/run/'
+				if input_size == 'simlarge':
+					inp += '400000.nets 128'
+				elif input_size == 'native':
+					inp += '2500000.nets 6000'
 				cmd = exe + ' ' + args + ' ' + inp + ' '
+
 			elif bench == 'dedup':
 				exe = pwd+'/pkgs/kernels/'+bench+'/obj/amd64-linux.gcc/'+bench+suffix
-				args = ' -c -p -v -t 12 '
-				inp = ' -i '+pwd+'/pkgs/kernels/dedup/run/media.dat'
+				args = ' -c -p -v -t ' + str(cores) 
+				inp = ' -i '+pwd+'/pkgs/kernels/dedup/run/'
+				if input_size == 'simlarge':
+					inp += 'media.dat'
+				elif input_size == 'native':
+					inp += 'FC-6-x86_64-disc1.iso'
 				outp = ' -o '+pwd+'/pkgs/kernels/dedup/run/output.dat.ddp'
 				cmd = exe + ' ' + args + ' ' + inp + ' ' + outp
+
 			elif bench == 'ferret':
 				if config != 'pthread':
 					suffix = '-pthreads-'+config
 				exe = pwd+'/pkgs/apps/'+bench+'/inst/amd64-linux.gcc/bin/'+bench+suffix
 				args = pwd+'/pkgs/apps/ferret/run/corel lsh '
-				inp = pwd+'/pkgs/apps/ferret/run/queries/ 10 20 12 '
+				inp = pwd+'/pkgs/apps/ferret/run/queries/'
+				if input_size == 'simlarge':
+					inp += ' 10 20 ' + str(cores) 
+				elif input_size == 'native':
+					inp += ' 50 20 ' + str(cores)
 				outp = pwd+'/pkgs/apps/ferret/run/output.txt'
 				cmd = exe + ' ' + args + ' ' + inp + ' ' + outp
+
 			elif bench == 'streamcluster':
 				exe = pwd+'/pkgs/kernels/streamcluster/obj/amd64-linux.gcc/streamcluster'
-				args = ' 10 20 128 16384 16384 1000 none '
-				outp = pwd+'/pkgs/kernels/streamcluster/run/output.txt 12'
+				if input_size == 'simlarge':
+					args = ' 10 20 128 16384 16384 1000 none '
+				elif input_size == 'native':
+					args = ' 10 20 128 1000000 200000 5000 none '
+				outp = pwd+'/pkgs/kernels/streamcluster/run/output.txt ' + str(cores)
 				cmd = exe + ' ' + args + ' ' + outp 
+
 			elif bench == 'swaptions':
-				exe = pwd+'/pkgs/apps/swaptions/obj/amd64-linux.gcc/swaptions '
-				args = ' -ns 64 -sm 40000 -nt 12 '
+				exe = pwd+'/pkgs/apps/swaptions/obj/amd64-linux.gcc/swaptions'
+				if input_size == 'simlarge':
+					args = ' -ns 64 -sm 40000 -nt ' + str(cores) 
+				elif input_size == 'native':
+					args = ' -ns 128 -sm 1000000 -nt ' + str(cores)
 				cmd = exe + ' ' + args
 
 			n = 0
@@ -107,21 +140,32 @@ def simlarge():
 
 # Print results
 def printStats(data):
-	for benchmark in benchmarks:
-		print (benchmark),
+	print 'benchmark',
+	for config in configs :
+		print '\t' + config,   
+	print
+	for bench in benchmarks:
+		print (bench),
 		for config in configs:
-			if benchmark in data and config in data[benchmark] and len(data[benchmark][config]) == runs:
-				if len(data[benchmark][config]) >= 4:
-					mean = (sum(data[benchmark][config])-max(data[benchmark][config])-min(data[benchmark][config]))/(runs-2)
+			if bench in data and config in data[bench] and len(data[bench][config]) == runs:
+				if len(data[bench][config]) >= 4:
+					mean = (sum(data[bench][config])-max(data[bench][config])-min(data[bench][config]))/(runs-2)
 				else:
-					mean = sum(data[benchmark][config])/runs
+					mean = sum(data[bench][config])/runs
 				print ('\t'+str(mean)),
 			else:
 				print ('\tNOT RUN'),
 		print 
 
+# Generate input files using parsec tool
+def generateInputs():
+	for bench in benchmarks:
+		cmd = pwd+'/bin/parsecmgmt -a run -p '+bench+' -i '+input_size+' -n '+str(cores)
+		os.system(cmd)
 
 def main():
-	data = simlarge()
-	printStats(data)
+#	generateInputs()
+	data = sim()
+#	printStats(data)
+
 main()
