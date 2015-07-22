3fc19e8d9c98d844252c0cb7742ef60295bb865a
Helge Bruegner <helge.bruegner@hp.com>
2015-07-22 14:50:29 -0700

    Added singlethreaded apps/pagerank, timer is not yet working

---
+++ apps/pagerank/logger.h
@@ -0,0 +1,44 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <time.h>
+
+/* --- LOG ---  */
+
+typedef unsigned int logd_lvl_t;
+
+static const logd_lvl_t LOGD_ALL = 0;
+static const logd_lvl_t LOGD_L = 1;
+static const logd_lvl_t LOGD_M = 500;
+static const logd_lvl_t LOGD_H = 1000;
+static const logd_lvl_t LOGD_NONE = UINT_MAX;
+
+extern void logd_set_level(logd_lvl_t lvl);
+
+extern void logd(logd_lvl_t lvl, const char *format, ...);
+
+extern void logd_e(const char *format, ...);
+
+/* --- TIMER --- */
+
+typedef long l_time_t;
+
+extern struct timespec* timer_alloc();
+
+extern void timer_start(struct timespec *tmr);
+
+extern l_time_t timer_total(struct timespec *tmr);
+
+extern l_time_t timer_total_ms(struct timespec *tmr);
+
+extern l_time_t timer_lap(struct timespec *tmr);
+
+extern l_time_t timer_lap_ms(struct timespec *tmr);
+
+extern void timer_free(struct timespec *tmr);
+
+#endif // LOGGER_H
