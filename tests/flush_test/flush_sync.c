88fc656a87fcb8b44e54f1650d89b2f87fd4cb43
Terry Hsu <hsuchi@120-1.bfc.labs.hpecorp.net>
2015-08-01 08:15:11 -0700

    Simple try on flush.

---
+++ tests/flush_test/flush_sync.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+unsigned long long TOUCH_SIZE = 2 * 1024 * 1024;
+int main(int argc, char **argv){
+	unsigned long i = 0;
+	int *a = (int*)malloc(TOUCH_SIZE);
+	int fd = open(argv[1], O_CREAT | O_WRONLY | O_ASYNC, 0664);
+	while (i < 1024 * 1024){
+		write(fd, (void*)a, TOUCH_SIZE);
+		i++;
+	}
+	fdatasync(fd);
+	close(fd);
+	return 0;
+}
