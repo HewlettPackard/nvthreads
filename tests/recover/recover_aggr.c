fa5e63e87aa6439887a45c15d16b19a061e0a26a
Terry Hsu <terry.shoes@gmail.com>
2015-10-14 17:38:35 -0400

    Modified the test case for recovery of aggregate data structures.  The test case touches different members in a struct locating in different pages while leaving some pages untouched.

--- tests/recover/recover_aggr.c
+++ tests/recover/recover_aggr.c
@@ -18,19 +18,37 @@ pthread_mutex_t gm;
 // aggregate data structures
 struct bar{
     int a;
-    char c[5000];
+    char c[15000];
 };
 struct foo{
     int id;
     struct bar b;
 };
 
+
+struct thread_data {
+    int tid;
+    struct foo *local_data;
+};
+
 void *t(void *args){
+    struct thread_data *data = (struct thread_data*)args;
+    int tid = data->tid;
+    struct foo *tmp = data->local_data;
+
+    pthread_mutex_lock(&gm);
+    tmp->b.c[97+tid] = 97+tid;    // 97 is 'a'
+    tmp->b.c[10000] = '!'+tid;
+    
+    pthread_mutex_unlock(&gm);
+ 
+    free(args);
     pthread_exit(NULL);
 }
 
 int main(){
-    pthread_t tid1;    
+    pthread_mutex_init(&gm, NULL);
+    pthread_t tid[3];    
     
     printf("Checking crash status\n");
     if ( isCrashed() ) {
@@ -40,22 +58,48 @@ int main(){
         nvrecover(f, sizeof(struct foo), (char *)"f");
 
         // Verify results
-        printf("f->b.c[4096] = %c\n", f->b.c[4096]);        
+        printf("f->b.c[97] = %c\n", f->b.c[97]);        
+        printf("f->b.c[98] = %c\n", f->b.c[98]);        
+        printf("f->b.c[99] = %c\n", f->b.c[99]);        
+        printf("f->b.c[10000] = %c\n", f->b.c[10000]);        
         printf("f->id = %d\n", f->id);
         free(f);
     }
     else{    
         printf("Program did not crash before, continue normal execution.\n");
-        pthread_create(&tid1, NULL, t, NULL); 
         
         // Assign magic numbers and character to the aggregate data structure
         struct foo *f = (struct foo*)nvmalloc(sizeof(struct foo), (char*)"f");
-        f->id = 12345;
-        f->b.c[4096] = '$';
-
+        memset(f->b.c, 0, 15000);
         printf("finish writing to values\n");
+                
+        int i;
+        for (i = 0; i < 3; i++) {
+            struct thread_data *tmp = (struct thread_data*)malloc(sizeof(struct thread_data));
+            tmp->tid = i;
+            tmp->local_data = f;
+            pthread_create(&tid[i], NULL, t, (void*)tmp);
+        }
 
-        pthread_join(tid1, NULL);
+        pthread_mutex_lock(&gm);
+        f->id = 12345;
+        f->b.c[10000] = '$';
+        printf("main wrote $ to %p\n", &f->b.c[10000]);
+        pthread_mutex_unlock(&gm);
+
+        pthread_mutex_lock(&gm);
+        f->b.c[10000] = '$';
+        printf("main wrote $ to %p again\n", &f->b.c[10000]);
+        pthread_mutex_unlock(&gm);
+
+        for (i = 0; i < 3; i++) {
+            pthread_join(tid[i], NULL);
+        }
+
+        printf("f->b.c[97]: %c\n", f->b.c[97]);
+        printf("f->b.c[98]: %c\n", f->b.c[98]);
+        printf("f->b.c[99]: %c\n", f->b.c[99]);
+        printf("f->b.c[10000]: %c\n", f->b.c[10000]);
 
         // Crash the program
         printf("internally abort!\n");
