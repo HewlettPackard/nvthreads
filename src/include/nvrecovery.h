7cf3379aceed877ed75de3e0128c1533a6b5b00a
Terry Hsu <terry.shoes@gmail.com>
2015-10-20 11:15:34 -0400

    Fixed typo in comments (NVTREADS -> NVTHREADS).

--- src/include/nvrecovery.h
+++ src/include/nvrecovery.h
@@ -2,20 +2,20 @@
 #define _NVRECOVERY_H_
 
 /*
-    This file is part of NVTREADS.
+    This file is part of NVTHREADS.
 
-    NVTREADS is free software: you can redistribute it and/or modify
+    NVTHREADS is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    NVTREADS is distributed in the hope that it will be useful,
+    NVTHREADS is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with NVTREADS.  If not, see <http://www.gnu.org/licenses/>.
+    along with NVTHREADS.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
