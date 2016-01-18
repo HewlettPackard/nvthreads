95bb7a0fe05992164004f56a65639df223b33933
Terry Hsu <terry.shoes@gmail.com>
2016-01-17 21:57:35 -0600

    Added crash test case for condition variable.

--- tests/mutex/condvar.c
+++ tests/mutex/condvar.c
@@ -1,48 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <pthread.h>
+#include "nvrecovery.h"
 pthread_mutex_t glock1;
 pthread_mutex_t glock2;
 pthread_cond_t cv;
 int A;
-int B;
+int *B;
 #define LIMIT_A 100
 void *thread_a(void *args){
         pthread_mutex_lock(&glock1);
-        printf("a locked glock 1\n");
 	sleep(1);
         pthread_mutex_lock(&glock2);
-        printf("a locked glock 2\n");
-	B = 99;
+	*B = 99;
 	sleep(1);
         while(A != 1){
 		sleep(1);
                 pthread_cond_wait(&cv, &glock2);
         }
-        printf("a read A=%d!\n", A);
-        printf("a unlocked glock 2\n");
         pthread_mutex_unlock(&glock2);
 	sleep(1);
-        printf("a unlocked glock 1\n");
+        printf("abort!\n");
+        abort();
         pthread_mutex_unlock(&glock1);
         return 0;
 }
 void *thread_b(void *args){
         sleep(1);
         pthread_mutex_lock(&glock2);
-        printf("b locked glock 2\n");
 	sleep(1);
-        pthread_cond_signal(&cv);
-	printf("B = %d\n", B);
         A = 1;
-        printf("b unlocked glock 2\n");
+        pthread_cond_signal(&cv);
+        *B = *B + A;
+	printf("B = %d\n", *B);
         pthread_mutex_unlock(&glock2);
         return 0;
 }
 int main(void){
         pthread_t tid1, tid2;
         A = 0;
-	B = 0;
+        B = (int *)nvmalloc(sizeof(int), (char*)"B");
+        if(isCrashed()) {
+                nvrecover(B, sizeof(int), (char*)"B");
+                printf("recovered B = %d\n", *B);
+                return 0;
+        } else{
+                *B = 0;
+        }
         pthread_mutex_init(&glock1, NULL);
         pthread_mutex_init(&glock2, NULL);
         pthread_cond_init(&cv, NULL);
@@ -50,7 +55,7 @@ int main(void){
         pthread_create(&tid2, NULL, thread_b, NULL);
         pthread_join(tid1, NULL);
         pthread_join(tid2, NULL);
-        printf("final A = %d\n", A);
+        printf("final A = %d, B= %d\n", A, *B);
         return 0;
 }
 
