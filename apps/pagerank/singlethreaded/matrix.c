a380b65c4fbfb6cf2b201c59b44358b03c9801c7
Terry Hsu <terry.shoes@gmail.com>
2016-04-10 15:28:38 -0400

    Added copyright to all versions of PageRank source.

--- apps/pagerank/singlethreaded/matrix.c
+++ apps/pagerank/singlethreaded/matrix.c
@@ -1,3 +1,20 @@
+/*
+  Copyright 2015-2016 Hewlett Packard Enterprise Development LP
+  
+  This program is free software; you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License, version 2 as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, write to the Free Software 
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+*/
 #include <matrix.h>
 
 extern void matrix_f_init(matrix_f* m, size_t size)
