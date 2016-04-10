c75bb7319328875a608776887c5b6fc3f10c4d38
Terry Hsu <terry.shoes@gmail.com>
2016-04-10 11:22:06 -0400

    Added copyright info.

--- src/include/logger.h
+++ src/include/logger.h
@@ -1,22 +1,23 @@
+/*
+  Copyright 2015-2016 Hewlett Packard Enterprise Development LP
+  
+  This program is free software; you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License, version 2 as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+  See the GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License 
+  along with this program; if not, write to the Free Software 
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+*/
+
 #ifndef _LOGGER_H_
 #define _LOGGER_H_
-
-/*
-    This file is part of NVTHREADS.
-
-    NVTHREADS is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    NVTHREADS is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with NVTHREADS.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 /*
  *  @file       logger.h
