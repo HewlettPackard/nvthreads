8f8c24ad9b4a1a18b8d6acb29ba0a00473fdc072
Terry Hsu <terry.shoes@gmail.com>
2016-05-10 19:21:30 -0500

    Test cases for the new nvsync engine.

--- tests/mutex/barrier.c
+++ tests/mutex/barrier.c
@@ -5,24 +5,27 @@
 #include "nvrecovery.h"
 #define NTHREADS 10
 pthread_barrier_t bar;
-
+int A;
 void* thread_a(void *args) {
-    int *tid = (int*)args;
+    A++;
     pthread_barrier_wait(&bar);
+    printf("%d: A=%d\n", getpid(), A);
 }
 int main(void) {
-    int i;
+    int i;    
     pthread_t tid[NTHREADS];
     printf("M: bar %p\n", &bar);
-    pthread_barrier_init(&bar, NULL, NTHREADS+1);
+    pthread_barrier_init(&bar, NULL, NTHREADS);
+    A = 0;
+    printf("main: A=%d\n", A);
     for(i = 0; i < NTHREADS; i++) {
         pthread_create(&tid[i], NULL, thread_a, NULL);
     }
-        sleep(2);
-    pthread_barrier_wait(&bar);
+
     for(i = 0; i < NTHREADS; i++) {
         pthread_join(tid[i], NULL);
     }
+    printf("main: A=%d\n", A);
     printf("M: exits\n");
     return 0;
 }
--- tests/mutex/condvar.c
+++ tests/mutex/condvar.c
@@ -3,59 +3,63 @@
 #include <unistd.h>
 #include <pthread.h>
 #include "nvrecovery.h"
+
 pthread_mutex_t glock1;
 pthread_mutex_t glock2;
 pthread_cond_t cv;
 int A;
 int *B;
 #define LIMIT_A 100
-void *thread_a(void *args){
-        pthread_mutex_lock(&glock1);
-	sleep(1);
-        pthread_mutex_lock(&glock2);
-	*B = 99;
-	sleep(1);
-        while(A != 1){
-		sleep(1);
-                pthread_cond_wait(&cv, &glock2);
-        }
-        pthread_mutex_unlock(&glock2);
-	sleep(1);
-        printf("abort!\n");
-        abort();
-        pthread_mutex_unlock(&glock1);
-        return 0;
-}
-void *thread_b(void *args){
+void* thread_a(void *args) {
+    pthread_mutex_lock(&glock1);
+    sleep(1);
+    printf("thread_a locked g1\n");
+    pthread_mutex_lock(&glock2);
+    printf("thread_a locked g2\n");
+    *B = 99;
+    sleep(1);
+    printf("thread_a waiting...\n");
+    while (A != 1) {
         sleep(1);
-        pthread_mutex_lock(&glock2);
-	sleep(1);
-        A = 1;
-        pthread_cond_signal(&cv);
-        *B = *B + A;
-	printf("B = %d\n", *B);
-        pthread_mutex_unlock(&glock2);
-        return 0;
+        pthread_cond_wait(&cv, &glock2);
+        printf("A = %d\n", A);
+    }
+    printf("thread_a is up!\n");
+    pthread_mutex_unlock(&glock2);
+    printf("thread_a unlocked g2\n");
+    sleep(1);
+    pthread_mutex_unlock(&glock1);
+    printf("thread_a unlocked g1\n");
+    return 0;
 }
-int main(void){
-        pthread_t tid1, tid2;
-        A = 0;
-        B = (int *)nvmalloc(sizeof(int), (char*)"B");
-        if(isCrashed()) {
-                nvrecover(B, sizeof(int), (char*)"B");
-                printf("recovered B = %d\n", *B);
-                return 0;
-        } else{
-                *B = 0;
-        }
-        pthread_mutex_init(&glock1, NULL);
-        pthread_mutex_init(&glock2, NULL);
-        pthread_cond_init(&cv, NULL);
-        pthread_create(&tid1, NULL, thread_a, NULL);
-        pthread_create(&tid2, NULL, thread_b, NULL);
-        pthread_join(tid1, NULL);
-        pthread_join(tid2, NULL);
-        printf("final A = %d, B= %d\n", A, *B);
-        return 0;
+void* thread_b(void *args) {
+    sleep(2);
+    pthread_mutex_lock(&glock2);
+    printf("thread_b locked g2\n");
+    sleep(5);
+    A = 1;
+    pthread_cond_signal(&cv);
+    *B = *B + A;
+    printf("A = %d, B = %d\n", A, *B);
+    pthread_mutex_unlock(&glock2);
+    printf("thread_b unlocked g2\n");
+    return 0;
+}
+int main(void) {
+    pthread_t tid1, tid2;
+    A = 0;
+    B = (int *)malloc(sizeof(int));
+    *B = 0;
+    printf("glock1: %p, glock2: %p, cond: %p\n", &glock1, &glock2, &cv);
+    pthread_mutex_init(&glock1, NULL);
+    pthread_mutex_init(&glock2, NULL);
+    pthread_cond_init(&cv, NULL);
+    pthread_create(&tid1, NULL, thread_a, NULL);
+    pthread_create(&tid2, NULL, thread_b, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid1, NULL);
+
+    printf("final A = %d, B= %d\n", A, *B);
+    return 0;
 }
 
