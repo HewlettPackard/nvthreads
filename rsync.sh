9c373bd51360971ced492a0fa3aa27318a2fa6e4
Terry Hsu <terry.shoes@gmail.com>
2017-02-15 16:07:46 -0600

    rsync script takes arguments.

--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,5 @@
-rsync -auv --rsh='ssh -p 9999 ' --exclude-from rsync_exclude ../nvthreads terry@localhost:/home/terry/workspace/
+#rsync -auv --rsh='ssh -p 9999 ' --exclude-from rsync_exclude ../nvthreads terry@localhost:/home/terry/workspace/
+rsync -auv --exclude-from rsync_exclude ../nvthreads $1@$2:$3
 OUT=$?
 if [ $OUT -eq 0 ];then
 	echo "[rsync completed]"
