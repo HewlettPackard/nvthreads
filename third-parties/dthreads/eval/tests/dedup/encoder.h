298a0d41d9d146bf47b25d8b53887cdbd377b1da
Terry Hsu <terry.shoes@gmail.com>
2017-03-08 10:28:40 -0600

    Removed Dthreads from third-parties.

--- third-parties/dthreads/eval/tests/dedup/encoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ENCODER_H_
-#define _ENCODER_H_ 1
-
-#define MIN_RABIN_CHUNK_SIZE (1*1024)
-#define MAX_RABIN_CHUNK_SIZE (60*1024)
-
-void Encode(config * conf);
-
-#endif /* !_ENCODER_H_ */
--- third-parties/dthreads/eval/tests/defines.mk
+++
@@ -1,5 +0,0 @@
-HOME=../..
-DATASET_HOME=../../datasets
-#<<<<<<< HEAD
-#=======
-#>>>>>>> ecea07c2d444b0454de49cadcabab0e17bb64ff6
